0000d6b7de347487929d9cad8b9d7134ab052e94
reece-vlr <reece.vlr@gmail.com>
2024-02-03 11:56:48 +0800

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-
-import ResponsiveAppBar from './components/ResponsiveAppBar'
-import Timeline from './components/Timeline';
 import './styles/App.css'
+import Timeline from './components/Timeline';
+import ResponsiveAppBar from './components/ResponsiveAppBar'
 
 // Components
 import LoginPage from './components/LoginPage';
@@ -12,22 +11,39 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { useThemeContext } from './theme/ThemeContextProvider'
 
+// Routes
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 function App() {
 	const { theme } = useThemeContext();
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <LoginPage></LoginPage>
+		},
+		{
+			path: "/login",
+			element: <LoginPage></LoginPage>
+		},
+		{
+			path: "/timeline",
+			element: <Timeline></Timeline>
+		}
+	])
 
 	return (
-		<ThemeProvider theme={ theme }>
-			<CssBaseline />
-			{/* Start of content */}
-			<div className="App">
-				{/* <LoginPage></LoginPage	> */}
-
-				<ResponsiveAppBar logoTitle={"DRRS"} />
-				<Timeline></Timeline>
-				<Footer />
-			</div>
-			{/* End of content */}
-		</ThemeProvider>
+		<>
+			<ThemeProvider theme={ theme }>
+					<CssBaseline />
+					{/* Start of content */}
+					<div className="App">
+						<ResponsiveAppBar logoTitle={"DRRS"} />
+							<RouterProvider router={router}></RouterProvider>
+						<Footer />
+					</div>
+					{/* End of content */}
+				</ThemeProvider>
+		</>
 	)
 }
 
