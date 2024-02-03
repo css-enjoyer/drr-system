dc772233680949adb80e53d26ae4074343405fb4
reece-vlr <reece.vlr@gmail.com>
2024-02-03 17:56:45 +0800

    Update signin with rerouting

--- src/components/logbuttons/LoginButton.tsx
+++ src/components/logbuttons/LoginButton.tsx
@@ -1,23 +1,36 @@
-import { signInWithPopup } from "firebase/auth";
-import { auth, googleProvider } from "../../firebase/config";
+import { useState } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom"
 import { Button } from "@mui/material";
 import { LoginSharp } from "@mui/icons-material";
 
 function Login() {
-    
-    const signInWithGoogle = async () => {
+    const auth = getAuth();
+    const navigate = useNavigate();
+    const [authing, setAuthing] = useState(false);
+
+    async function signInWithGoogle() {
+        setAuthing(true);
+
         try {
-            await signInWithPopup(auth, googleProvider);
-            console.log(auth?.currentUser?.email);
-            console.log(auth?.currentUser?.photoURL);
-            console.log(auth?.currentUser?.displayName);
-        } catch (err) {
-            console.error(err);
+            await signInWithPopup(auth, new GoogleAuthProvider());
+            console.log(auth?.currentUser?.uid);
+            navigate('/timeline');
+        } catch (error) {
+            console.error(error);
+            setAuthing(false);
         }
-    };
+    }
 
     return (
-        <Button onClick={signInWithGoogle} variant="contained" endIcon={<LoginSharp />}>Sign in with Google</Button>
+        <Button 
+            onClick={signInWithGoogle} 
+            variant="contained" 
+            endIcon={<LoginSharp />}
+            disabled={authing}>
+            Sign in with Google
+        </Button>
     )
 }
+
 export default Login;
