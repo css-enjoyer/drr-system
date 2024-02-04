1b85ff7371fc71de72adc154ab68029ed90e4cc5
Isaac <isaacjdjreyes@gmail.com>
2024-02-04 10:11:32 +0800

    Remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeContextProvider } from './theme/ThemeContextProvider.tsx'
-import { AuthContext, AuthContextProvider } from './utils/AuthContext.tsx'
+import { AuthContextProvider } from './utils/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<ThemeContextProvider>
