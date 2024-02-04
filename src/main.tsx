a0ce5b843d2692e00af1391673d2b0bbdc485082
Isaac <isaacjdjreyes@gmail.com>
2024-02-04 10:10:33 +0800

    Wrap main.tsx with AuthContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeContextProvider } from './theme/ThemeContextProvider.tsx'
+import { AuthContext, AuthContextProvider } from './utils/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<ThemeContextProvider>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		<AuthContextProvider>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</AuthContextProvider>
 	</ThemeContextProvider>
 )
