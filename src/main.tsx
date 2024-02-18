6d5d23db2c71db7b7779bca0e102f2f3d751a564
Edriech Balajadia <edriechbalajadia@gmail.com>
2024-02-18 23:51:54 +0800

    disable react strict mode since it causes data to be fetched twice

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { ThemeContextProvider } from './theme/ThemeContextProvider.tsx'
-import { AuthContextProvider } from './utils/AuthContext.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeContextProvider } from "./theme/ThemeContextProvider.tsx";
+import { AuthContextProvider } from "./utils/AuthContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<ThemeContextProvider>
-		<BrowserRouter>
-			<AuthContextProvider>
-				<React.StrictMode>
-					<App />
-				</React.StrictMode>
-			</AuthContextProvider>
-		</BrowserRouter>
-	</ThemeContextProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <ThemeContextProvider>
+        <BrowserRouter>
+            <AuthContextProvider>
+                {/* <React.StrictMode> */}
+                <App />
+                {/* </React.StrictMode> */}
+            </AuthContextProvider>
+        </BrowserRouter>
+    </ThemeContextProvider>
+);
