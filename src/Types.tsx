dc49ed6a2a16d51bb09b4b7764a7215d215a7fa3
Edriech Balajadia <edriechbalajadia@gmail.com>
2024-02-22 17:49:04 +0800

    clean code

---
+++ src/Types.tsx
@@ -0,0 +1,72 @@
+export interface Reservation {
+    branchId: string;
+    roomId: number;
+    logDate: Date;
+    logStuRep: string;
+    logStart: Date;
+    logEnd: Date;
+    logPurp: string;
+    logPax: number;
+    logRcpt: string;
+}
+
+// reservation event
+export interface ReservationEvent {
+    // required types for events
+    event_id: number;
+    title: string;
+    start: Date;
+    end: Date;
+
+    branchId: string;
+    roomId: number;
+    logDate: Date;
+    logStuRep: string;
+    logPurp: string;
+    logPax: number;
+    logRcpt: string;
+}
+
+export interface Branch {
+    branchId: string;
+    branchTitle: string;
+    branchLoc: string;
+}
+
+export interface Room {
+    roomId: number;
+    roomTitle: string;
+    roomPax: number;
+    roomAvailable: boolean;
+    roomBranch: string;
+}
+
+export interface BranchRoom {
+    roomId: number;
+    roomBranch: string;
+    roomName: string;
+    roomPax: number;
+    roomAvailable: boolean;
+}
+
+// ------ Prop Types -----
+
+export type RoomProps = {
+    room_id: number,
+    roomBranch: string,
+    title: string,
+    color: string,
+}
+
+export type EventProps = {
+    event_id: number,
+    room_id: number,
+    title: string
+    start: Date,
+    end: Date
+}
+
+export type DurationOption = {
+    duration: number,
+    label: string
+}
---
+++ src/utils/Utils.ts
@@ -0,0 +1,20 @@
+// used to generate receipts
+export function generateRandomSequence() {
+    const length = 10;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomSequence = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomSequence += characters.charAt(randomIndex);
+    }
+
+    return randomSequence;
+}
+
+export function toTitleCase(inputString: string | null | undefined) {
+    if (inputString === null || inputString === undefined) {
+        return "";
+    }
+    return inputString.toLowerCase().replace(/(?:^|\s)\w/g, match => match.toUpperCase());
+}
