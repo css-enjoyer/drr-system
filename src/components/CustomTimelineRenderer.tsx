a3f4b7a35cab10deedc813bda6d76ece980c3ff2
reece-vlr <reece.vlr@gmail.com>
2024-03-08 20:38:07 +0800

    Update to accommodate new function

--- src/components/CustomTimelineRenderer.tsx
+++ src/components/CustomTimelineRenderer.tsx
@@ -162,18 +162,18 @@ function CustomTimelineRenderer({ branchId }: { branchId: string }) {
                     const addResRoomId = formState.roomId;
                     const addResStart = formState.start;
                     const addResEnd = formState.end;
-                    const formattedResDate = formatDate(
-                        addResStart.getDate(), 
-                        addResStart.getMonth(), 
-                        addResStart.getFullYear()
-                    );
+                    // const formattedResDate = formatDate(
+                    //     addResStart.getDate(), 
+                    //     addResStart.getMonth(), 
+                    //     addResStart.getFullYear()
+                    // );
 
                     const overlapping = isReservationOverlapping(
                         eventsState,
                         addResStart,
                         addResEnd,
                         addResRoomId,
-                        formattedResDate,
+                        // formattedResDate
                     );
 
                     if (!overlapping) {
@@ -190,18 +190,18 @@ function CustomTimelineRenderer({ branchId }: { branchId: string }) {
                     const editResRoomId = newResEvent.room_id;
                     const editResStart = newResEvent.start;
                     const editResEnd = newResEvent.end;
-                    const formattedResDate = formatDate(
-                        newResEvent.start.getDate().toString(), 
-                        newResEvent.start.getMonth().toString(), 
-                        newResEvent.start.getFullYear().toString()
-                    );
+                    // const formattedResDate = formatDate(
+                    //     newResEvent.start.getDate().toString(), 
+                    //     newResEvent.start.getMonth().toString(), 
+                    //     newResEvent.start.getFullYear().toString()
+                    // );
 
                     const overlapping = isReservationOverlapping(
                         eventsState,
                         editResStart,
                         editResEnd,
                         editResRoomId,
-                        formattedResDate,
+                        // formattedResDate,
                         true
                     );
 
