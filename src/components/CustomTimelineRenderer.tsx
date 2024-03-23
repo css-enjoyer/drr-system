5deecbaed1c1b619297ff87dc24f01c2d57537d3
reece-vlr <reece.vlr@gmail.com>
2024-03-23 17:27:08 +0800

    Add duration option of whole day

--- src/components/CustomTimelineRenderer.tsx
+++ src/components/CustomTimelineRenderer.tsx
@@ -7,12 +7,9 @@ import { AuthContext } from "../utils/AuthContext";
 import { addReservationEvent, deleteReservationEvent, editReservationEvent, editReservationEventTitle, getReservationEvents, getRooms } from "../firebase/dbHandler";
 import { TimePicker } from "@mui/x-date-pickers";
 import { DurationOption, ReservationEvent, RoomProps } from "../Types";
-import { generateRandomSequence, isReservationBeyondOpeningHrs, isReservationOverlapping, isStudentReservationConcurrent } from "../utils/Utils.ts"
+import { generateRandomSequence, isReservationBeyondOpeningHrs, isReservationOverlapping, isStudentReservationConcurrent, isWholeDay, setDurationOptions } from "../utils/Utils.ts"
 import { Numbers, Portrait, TextSnippet } from "@mui/icons-material";
 import Loading from "./miscellaneous/Loading";
-
-const durationOptions: DurationOption[] = [{ duration: 30, label: "30 Minutes" }, { duration: 60, label: "1 Hour" }, { duration: 90, label: "90 Minutes" }, { duration: 120, label: "2 Hours" }]
-
 
 function CustomTimelineRenderer({ branchId }: { branchId: string }) {
     const timelineRef = useRef<SchedulerRef>(null);
@@ -20,6 +17,15 @@ function CustomTimelineRenderer({ branchId }: { branchId: string }) {
     // console.log(timelineRef);
 
     const authContext = useContext(AuthContext);
+
+    const startTime = 8;
+    const endTime = 17;
+
+    const durationOptions: DurationOption[] = setDurationOptions(
+        authContext?.userRole,
+        startTime,
+        endTime
+    );
 
     const [roomsState, setRoomsState] = useState<RoomProps[]>([]);
     const [eventsState, setEventsState] = useState<ProcessedEvent[]>([]);
@@ -364,7 +370,9 @@ function CustomTimelineRenderer({ branchId }: { branchId: string }) {
             view="day"
             events={eventsState}
             day={{
-                startHour: 8, endHour: 21, step: 30,
+                startHour: startTime, 
+                endHour: endTime, 
+                step: 30,
             }}
             resources={roomsState}
             resourceFields={{ idField: "room_id", textField: "title", }}
