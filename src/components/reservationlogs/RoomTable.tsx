439868eaa3a8bf244e7e63c3f7327e27f07036c4
reece-vlr <reece.vlr@gmail.com>
2024-04-12 15:21:06 +0800

    Update wrapper for consistency

--- src/components/reservationlogs/RoomTable.tsx
+++ src/components/reservationlogs/RoomTable.tsx
@@ -236,7 +236,6 @@ const RoomTable: React.FC<RoomTableProps> = ({ selectedBranch }) => {
 
     return (
         <div style={{ textAlign: "center", marginTop: "20px" }}>
-            <h3 style={{ marginBottom: "20px" }}>Room Management</h3>
             <div
                 style={{
                     marginBottom: "20px",
@@ -245,32 +244,39 @@ const RoomTable: React.FC<RoomTableProps> = ({ selectedBranch }) => {
                     alignItems: "center",
                     width: "100%",
                     marginLeft: "0%",
-                }}
-            >
-                <TextField
-                    label="Search"
-                    variant="outlined"
-                    value={searchQuery}
-                    onChange={handleSearchChange}
-                    style={{ flex: 1, marginRight: "20px" }}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleAddBranch}
-                    sx={{
-                        textTransform: "none",
-                        "@media (max-width: 600px)": {
-                            margin: "0px 0",
-                        },
-                    }}
-                    disabled={!selectedBranch}
-                >
-                    Add Room
-                </Button>
+                }}>
+                <div
+                    style={{
+                    marginBottom: "20px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    width: "90%",
+                    marginLeft: "5%",
+                    }}>
+                    <TextField
+                        label="Search"
+                        variant="outlined"
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                        style={{ flex: 1, marginRight: "20px" }}/>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleAddBranch}
+                        sx={{
+                            textTransform: "none",
+                            "@media (max-width: 600px)": {
+                                margin: "0px 0",
+                            },
+                        }}
+                        disabled={!selectedBranch}>
+                        Add Room
+                    </Button>
+                </div>
             </div>
 
-            <Paper sx={{ width: "100%", marginLeft: "0%", marginBottom: "60px" }}>
+            <Paper sx={{ width: "90%", marginLeft: "5%", marginBottom: "60px" }}>
                 <TableContainer
                     sx={{ maxHeight: "calc(150vh - 350px)", overflowX: "auto", overflowY: "hidden" }}
                 >
