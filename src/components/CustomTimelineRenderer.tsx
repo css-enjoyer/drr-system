9907bd09b642ca97d4b8ca761a947198a9386d6b
reece-vlr <reece.vlr@gmail.com>
2024-04-13 22:04:51 +0800

    Update reqd params of function

--- src/components/CustomTimelineRenderer.tsx
+++ src/components/CustomTimelineRenderer.tsx
@@ -52,8 +52,10 @@ function CustomTimelineRenderer({ branchId }: { branchId: string }) {
 
     const authContext = useContext(AuthContext);
 
+    // --- TIMELINE: START & ENDING HOUR --- //
     const startTime = 8;
-    const endTime = 18;
+    const endTime = 23;
+
     const [roomsState, setRoomsState] = useState<RoomProps[]>([]);
     const [eventsState, setEventsState] = useState<ProcessedEvent[]>([]);
     const [loading, setLoading] = useState(false);
@@ -296,7 +298,7 @@ function CustomTimelineRenderer({ branchId }: { branchId: string }) {
                 return
             }
 
-            if (isReservationBeyondOpeningHrs(formState.end)) {
+            if (isReservationBeyondOpeningHrs(formState.end, endTime)) {
                 setErrorMessage("Error! Your reservation exceeds library hours.");
                 return;
             }
