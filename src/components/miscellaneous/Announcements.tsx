762d36c02ef58f2246a299b55a4753e03d214af7
reece-vlr <reece.vlr@gmail.com>
2024-04-18 17:10:24 +0800

    Add front-end
    - Add simple UI based on FAQs
    - Sort by most recent date

--- src/components/miscellaneous/Announcements.tsx
+++ src/components/miscellaneous/Announcements.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Announcement } from '../../Types';
+import { Announcement, OpenState } from '../../Types';
 import { getAnnouncements } from '../../firebase/dbHandler';
-import { Box, Container, Typography } from '@mui/material';
+import { Add as AddIcon, Close as CloseIcon } from '@mui/icons-material';
+import { Box, Collapse, Container, Grid, IconButton, Paper, Typography } from '@mui/material';
 
 const Announcements = () => {
-    const [Announcement, setAnnouncement] = useState<Announcement[]>([]);
+    const [announcement, setAnnouncement] = useState<Announcement[]>([]);
+    const [open, setOpen] = useState<OpenState>({});
 
     useEffect(() => {
         const fetchAnnouncements = async() => {
@@ -19,8 +21,10 @@ const Announcements = () => {
                 content: announcement.content
             }));
 
+            // Sort announcements from most recent
+            transformedResources.sort((a, b) => b.dateCreation.getTime() - a.dateCreation.getTime()); 
             setAnnouncement(transformedResources);
-        }
+        };
         
         fetchAnnouncements();
     }, []);
@@ -37,7 +41,31 @@ const Announcements = () => {
     function editAnnouncement() {
         // ADD: Edit functionality
     };
-    
+
+    /* Helper Functions */
+    function handleToggle(index: number) {
+        setOpen((prevOpen: OpenState) => ({ 
+            ...prevOpen, 
+            [index]: !prevOpen[index] 
+        }));
+    }
+
+    function formatDate(date: Date) {
+        const format: Intl.DateTimeFormatOptions = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        };
+
+        const formattedDate: string = date.toLocaleDateString("en-US", format);
+
+        return formattedDate;
+    }
+
     return (
         <Box sx={{ minHeight: '100vh', bgcolor: 'background.default', py: 6 }}>
             <Container maxWidth="lg">
@@ -45,9 +73,32 @@ const Announcements = () => {
                     Announcements
                 </Typography>
             </Container>
+            <Grid container spacing={4}>
+                {announcement.map((announcement, index) => (
+                <Grid item xs={12} key={index}>
+                    <Paper elevation={3} sx={{ p: 3, paddingRight: '48px', position: 'relative' }}>
+                        <Typography variant="h6" sx={{ mb: 2 }}>
+                            { formatDate(announcement.dateCreation) }
+                        </Typography>
+                        <Typography variant="h4" sx={{ mb: 2 }}>
+                            { announcement.heading }
+                        </Typography>
+                        {/* ADD: BUTTON TO EDIT / DELETE FAQ IF USER IS LIBRARIAN */}
+                        <IconButton
+                            sx={{ position: 'absolute', top: '10px', right: '10px', zIndex: 1 }}
+                            onClick={() => handleToggle(index)}>
+                            { open[index] ? <CloseIcon /> : <AddIcon /> }
+                        </IconButton>
+                        <Collapse in={open[index]}>
+                            <Typography variant="body1">
+                                { announcement.content }
+                            </Typography>
+                        </Collapse>
+                    </Paper>
+                </Grid>
+                ))}
+            </Grid>
         </Box>
-
-        // ADD: FrontEnd & Add/Edit/Delete buttons
     );
 };
 
