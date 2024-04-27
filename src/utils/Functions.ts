85b8679a3d7241ce579b2f76db7ba449a6e46d89
Edriech Balajadia <edriechbalajadia@gmail.com>
2024-04-27 16:02:13 +0800

    fix negative mins when sending email notif and auto cancel

--- src/utils/Functions.ts
+++ src/utils/Functions.ts
@@ -14,22 +14,24 @@ export async function sendReminderEmail(start: Date, stuRep: string, roomId: num
         delaySent = minsBeforeRes - minsBeforeNotif;
     }
 
-    try {
-        const response = await axios.post(`${VITE_BACKEND_URL}/api/functions/email`,
-            {
-                to: stuRep,
-                subject: "DRRS: Upcoming reservation",
-                html: `<h1>${(minsBeforeRes > minsBeforeNotif && minsBeforeNotif > 0) ? minsBeforeNotif : minsBeforeRes} minutes before your reservation in room ${roomId} at ${branchId}</h1>`,
-                minutesDelay: delaySent,
-            },
-            {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-        console.log(`SUCCESS: Reservation reminder sent to: ${stuRep} after ${delaySent} minutes delay`);
-    } catch (error) {
-        console.error(error);
+    if (minsBeforeRes > 0) {
+        try {
+            const response = await axios.post(`${VITE_BACKEND_URL}/api/functions/email`,
+                {
+                    to: stuRep,
+                    subject: "DRRS: Upcoming reservation",
+                    html: `<h1>${(minsBeforeRes > minsBeforeNotif && minsBeforeNotif > 0) ? minsBeforeNotif : minsBeforeRes} minutes before your reservation in room ${roomId} at ${branchId}</h1>`,
+                    minutesDelay: delaySent,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })
+            console.log(`SUCCESS: Reservation reminder sent to: ${stuRep} after ${delaySent} minutes delay`);
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
 
@@ -41,25 +43,27 @@ export async function autoCancel(token: string, eventId: string, start: Date, st
     console.log("MINUTES BEFORE RESERVATION IS CANCELLED");
     console.log(cancellationDelay);
 
-    try {
-        console.log(`Processing: Auto cancel has been set ${stuRep} after ${cancellationDelay} minutes delay`);
-        const response = await axios.post(`${VITE_BACKEND_URL}/api/functions/autoCancel`,
-            {
-                eventId: eventId,
-                to: stuRep,
-                subject: "DRRS: Reservation Cancellation",
-                html: `<h1>Your reservation has been cancelled after not being occupied for ${minsAfterCancellation} minutes in room ${roomId} at ${branchId}</h1>`,
-                minutesDelay: cancellationDelay,
-            },
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`
-                }
-            });
-        console.log(`Succes: Auto cancel has been set ${stuRep} after ${cancellationDelay} minutes delay is done!`);
-    } catch (error) {
-        console.error(error);
+    if (cancellationDelay > 0) {
+        try {
+            console.log(`Processing: Auto cancel has been set ${stuRep} after ${cancellationDelay} minutes delay`);
+            const response = await axios.post(`${VITE_BACKEND_URL}/api/functions/autoCancel`,
+                {
+                    eventId: eventId,
+                    to: stuRep,
+                    subject: "DRRS: Reservation Cancellation",
+                    html: `<h1>Your reservation has been cancelled after not being occupied for ${minsAfterCancellation} minutes in room ${roomId} at ${branchId}</h1>`,
+                    minutesDelay: cancellationDelay,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+            console.log(`Succes: Auto cancel has been set ${stuRep} after ${cancellationDelay} minutes delay is done!`);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
 }
